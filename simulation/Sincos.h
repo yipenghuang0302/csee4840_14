9e0f37fe474936fc6f884d8705d7b752a8fb58a5
Yipeng Huang <yipeng@cs.columbia.edu>
2014-04-01 13:10:12 -0400

    Adding a draft of sincos generator.

---
+++ simulation/Sincos.h
@@ -0,0 +1,13 @@
+#ifndef __SINCOSH__
+#define __SINCOSH__
+
+#define _USE_MATH_DEFINES
+
+#include <math.h>
+#include <stdlib.h>
+#include <assert.h>
+
+// angles must be in radians
+void sincos (short angle);
+
+#endif
