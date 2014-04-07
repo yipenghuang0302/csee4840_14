3ef05f8bf026efdc6cfeec16ed3cf5f82ed13179
Yipeng Huang <yipeng@cs.columbia.edu>
2014-04-07 18:19:05 -0400

    Testbench.

--- rtl/sincos/top.sv
+++ rtl/sincos/top.sv
@@ -3,31 +3,28 @@
 
 `timescale 1ns/1ps
 
-module top_sincos ();
+module sincos_tb ();
 
-	logic clk = 0;
-	always #1 clk = ~clk;
+	logic clk, reset;
+	logic [2:0] count;
 
-	initial $vcdpluson;
+	initial begin
+		clk = 0;
+		forever
+			#20ns clk = ~clk;
+	end
+
+	initial begin // Reset
+		reset = 0;
+		repeat (2)
+			@(posedge clk);
+			reset = 1;
+		repeat (2)
+			@(posedge clk);
+			reset = 0;
+	  end
 
 	ifc_sincos ifc_sincos (clk);
 	sincos sincos (ifc_sincos);
-//	sincos_tb sincos_tb (ifc_sincos);
 
-endmodule
-
-
-module count3_tb; logic clk, reset; logic [2:0] count;
-count3 dut(.*);
-initial begin
-clk = 0;
-forever
-#20ns clk = ~clk;
-end
-initial begin // Reset
-reset = 0; repeat (2)
-@(posedge clk); reset = 1;
-repeat (2)
-@(posedge clk); reset = 0;
-  end
 endmodule
