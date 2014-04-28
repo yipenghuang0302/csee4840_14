61675c5653b17d8042e1a406e2fabd2194f09c5b
knithacker <lianne.lairmore@gmail.com>
2014-04-28 16:33:40 -0400

    more in style with everyone elses code

--- rtl/inverse/inverse_top.sv
+++ rtl/inverse/inverse_top.sv
@@ -1,64 +1,129 @@
-module inverse_top(input clk,
-                   input [5:0][5:0][26:0] matrix,
-                   input en,
-                   output [5:0][5:0][26:0] inverse,
-                   output mat_en,
-                   output rst,
-                   output mat_mode,
-                   output [5:0][5:0][26:0] mult_a,
-                   output [5:0][5:0][26:0] mult_b,
-                   input [5:0][5:0][26:0] result);
+module inverse_top(
+	ifc_jacobian.jacobian i
+);
 
-	parameter SQRT_COUNT = 5;
-  parameter DIV_COUNT = 5;
-  parameter MULT_ADD_COUNT = 7;
+  //parameter SQRT_COUNT = 5;
+  //parameter DIV_COUNT = 5;
+  //parameter MULT_ADD_COUNT = 6;
 
-	logic [6:0] count;
-	logic [4:0] inv_step;
-	logic [4:0] cholesky_step;
+    logic [4:0] inv_step;
+    logic [4:0] cholesky_step;
 
-	logic curr_mat_en, curr_rst;
-	logic [5:0][5:0][26:0] curr_mult_a;
-  logic [5:0][5:0][26:0] curr_mult_b;
 
-  assign mat_en = curr_mat_en;
-  assign rst = curr_rst;
-	assign mult_a = curr_mult_a;
-	assign mult_b = curr_mult_b;
-	assign mat_mode = 0;
 
-  logic div_en;
-  logic [4:0][26:0] dividends, quotients;
-  logic [26:0] divisor;
+	// logic to calculate inv_step
+	always_ff @(posedge i.clk)begin
+		case(i.count)
+			8'd0: inv_step <= 5'd0;
+			8'd10: inv_step <= 5'd1;
+			8'd16: inv_step <= 5'd2;
+			8'd22: inv_step <= 5'd3;
+            8'd34: inv_step <= 5'd4;
+            8'd45: inv_step <= 5'd5;
+            8'd51: inv_step <= 5'd6;
+            8'd63: inv_step <= 5'd7;
+            8'd69: inv_step <= 5'd8;
+            8'd80: inv_step <= 5'd9;
+            8'd86: inv_step <= 5'd10;
+            8'd92: inv_step <= 5'd11;
+            default: inv_step <= inv_step;
+		endcase
+	end
+
+    // logic to calculate cholesky_step
+    always_ff @(posedge i.clk)begin
+		case(i.count)
+			8'd0: cholesky_step <= 5'd0;
+            8'd1: cholesky_step <= 5'd1;
+            8'd5: cholesky_step <= 5'd2;
+			8'd10: cholesky_step <= 5'd3;
+			8'd16: cholesky_step <= 5'd4;
+			8'd22: cholesky_step <= 5'd5;
+			8'd28: cholesky_step <= 5'd6;
+            8'd34: cholesky_step <= 5'd7;
+            8'd40: cholesky_step <= 5'd8;
+            8'd45: cholesky_step <= 5'd9;
+            8'd51: cholesky_step <= 5'd10;
+            8'd57: cholesky_step <= 5'd11;
+            8'd63: cholesky_step <= 5'd12;
+            8'd69: cholesky_step <= 5'd13;
+            8'd75: cholesky_step <= 5'd14;
+            8'd80: cholesky_step <= 5'd15;
+            8'd86: cholesky_step <= 5'd16;
+            default: cholesky_step <= cholesky_step;
+		endcase
+	end
+   
+    logic div_en;
+    logic div_rst;
+    logic [4:0][26:0] dividends, quotients;
+    logic [26:0] divisor;
 
 	logic [5:0][5:0] [26:0] inverse_in;
 
-	logic inv_mat_en, inv_rst;
 	logic [5:0][5:0][26:0] inv_mult_a;
 	logic [5:0][5:0][26:0] inv_mult_b;
 
-  logic inv_div_en;
-  logic [4:0][26:0] inv_dividends;
-  logic [26:0] inv_divisor;
+    logic [4:0][26:0] inv_dividends;
+    logic [26:0] inv_divisor;
   
-  inverse inv(clk, inverse_in, inverse, inv_step, 
-          inv_mat_en, inv_rst, inv_mult_a, inv_mult_b, result,
-          inv_div_en, inv_dividends, inv_divisor, quotients);
+    inverse inv(clk, inverse_in, inverse, inv_step, 
+           inv_mult_a, inv_mult_b, array_mult_result,
+          inv_dividends, inv_divisor, quotients);
 
-  logic chol_mat_en, chol_rst;
-  logic [5:0][5:0][26:0] chol_mult_a;
-  logic [5:0][5:0][26:0] chol_mult_b;
+    logic [5:0][5:0][26:0] chol_mult_a;
+    logic [5:0][5:0][26:0] chol_mult_b;
 
-	logic chol_div_en;
-  logic [4:0][26:0] chol_dividends;
-  logic [26:0] chol_divisor;
+    logic [4:0][26:0] chol_dividends;
+    logic [26:0] chol_divisor;
 
 	cholesky chol(clk, cholesky_step, matrix, inverse_in, 
-                chol_mat_en, chol_rst, chol_mult_a, chol_mult_b, result,
-                chol_div_en, chol_ dividends, chol_divisor, quotients);
+                 chol_mult_a, chol_mult_b, result,
+                 chol_dividends, chol_divisor, quotients);
 
- 	divider div(clk, div_en, [4:0][26:0] dividends, [26:0] divisor, 
-		[4:0][26:0] quotients);
+    assign div_en = 1;
+    assign div_rst = 0;
+
+ 	array_div div(clk, div_en, div_rst, dividends, divisor, 
+		 quotients);
+ 
+    always_ff @(posedge i.clk)begin
+        if(i.count==8'd5 || i.count==8'd22 || 
+           i.count==8'd40 || i.count==8'd57 ||
+           i.count==8'd75) begin
+            dividends <= chol_dividends;
+            divisor <= chol_divisor;
+        end
+        else if(i.count==8'd10 || i.count==8'd28 ||
+                i.count==8'd45 || i.count==8'd63 || 
+                i.count==8'80) begin
+            dividends <= inv_dividends;
+            divisor <= inv_divisor;
+        end
+        else begin
+            dividends <= {5{27'b0}};
+        end
+    end
+
+    always_ff @(posedge i.clk)begin
+        if(i.count==8'd10 || i.count==8'd28 || 
+           i.count==8'd45 || i.count==8'd63 ||
+           i.count==8'd80) begin
+            i.array_mult_dataa <= chol_mult_a;
+            i.array_mult_datab <= chol_mult_b;
+        end
+        else if(i.count==8'd16 || i.count==8'd34 ||
+                i.count==8'd51 || i.count==8'd69 || 
+                i.count==8'86) begin
+            i.array_mult_dataa <= inv_mult_a;
+            i.array_mult_datab <= inv_mult_b;
+        end
+        else begin
+            i.array_mult_dataa <= {9{27'b0}};
+            i.array_mult_datab <= {9{27'b0}};
+        end
+    end
+
 
 	always@(posedge clk)begin
 		if(en && count == 0) begin  // 1
