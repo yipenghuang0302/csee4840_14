7f95a91af4cc9760828849a0f21ac12a2e59811c
Yipeng Huang <yipeng@cs.columbia.edu>
2014-05-07 18:00:46 -0400

    Continuing the state gating for all full jacobian branch of the hardware.

--- rtl/ik_swift_36/full_jacobian/top.sv
+++ rtl/ik_swift_36/full_jacobian/top.sv
@@ -10,6 +10,7 @@
 `include "../mat_mult/mat_mult_interface.sv"
 `include "../mat_mult/mat_mult.sv"
 `include "../mat_mult/mult_array.sv"
+`include "../mat_mult/mult_36_dsp/mult_36_dsp.v"
 
 `include "../array_mult/array_mult_interface.sv"
 `include "../array_mult/array_mult.sv"
@@ -65,9 +66,9 @@ module full_jacobian_top ();
 	ifc_mat_mult ifc_mat_mult (clk);
 	assign ifc_mat_mult.en = ifc_full_jacobian.en;
 	// delay rst for mat_mult by five
-	always_ff @(posedge clk) begin
-		ifc_mat_mult.rst <= ifc_full_jacobian.count==8'd28 || ifc_full_jacobian.count==8'd98;
-	end
+	always_ff @(posedge clk)
+		if ( ifc_full_jacobian.en )
+			ifc_mat_mult.rst <= ifc_full_jacobian.count==8'd28 || ifc_full_jacobian.count==8'd98;
 	assign ifc_mat_mult.mat_mode = 8'd90<=ifc_full_jacobian.count&&ifc_full_jacobian.count<8'd99 ? 1'b0 : 1'b1;
 	assign ifc_mat_mult.dataa = ifc_full_jacobian.mat_mult_dataa;
 	assign ifc_mat_mult.datab = ifc_full_jacobian.mat_mult_datab;
